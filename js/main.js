d7cefdbcfe1ffc91d1d74ae61aa0b73776df7358
shantanu-adiuvaret <shantanu.adiuvaret.kids@gmail.com>
2025-01-10 14:48:08 +0530

    all the text changes and image changes made

--- js/main.js
+++ js/main.js
@@ -78,16 +78,16 @@
     });
 
 
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
+    // gallery isotope and filter
+    var galleryIsotope = $('.gallery-container').isotope({
+        itemSelector: '.gallery-item',
         layoutMode: 'fitRows'
     });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
+    $('#gallery-flters li').on('click', function () {
+        $("#gallery-flters li").removeClass('active');
         $(this).addClass('active');
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+        galleryIsotope.isotope({filter: $(this).data('filter')});
     });
     
 })(jQuery);
